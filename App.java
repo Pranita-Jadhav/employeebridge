fd4c8bb6c7af5171e1fe95826639ddfa5c7501f3
Pranita-Jadhav <pranita.jadhav@mindbowser.com>
2021-05-12 09:11:33 +0530

    added

---
+++ App.java
@@ -0,0 +1,16 @@
+package com.java_programs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Local....!" );
+
+        
+    }
+}
